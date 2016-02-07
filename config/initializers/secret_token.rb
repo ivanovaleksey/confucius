59e526cb41a6565f36f9c15f84eb6e08188bd33a
Alexey Ivanov <Alexey.Ivanov@bfoxint.com>
2016-02-07 19:28:45 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Confucius::Application.config.secret_token = '74b97672a5d60ea5ed7938510fd0e15ae66bd12cf6a1cc72745e07d60ad664273a1f874b41239ca66a3f1089b4ed898c3ff179af6fe3516fb9236dd38b689167'
